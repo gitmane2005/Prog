7eee890438e41fb94384966b29844840dbfda274
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-02-22 11:54:56 +0000

    update

--- P01/md.cpp
+++ P01/md.cpp
@@ -4,15 +4,20 @@ int main(){
     int cycle;
     cin >> cycle;
     int x = 0;
-    int temx = 0;
+    cin >> x;
     int y = 0;
+    cin >> y;
+
+    int temx = 0;
     int temy = 0;
+    int distance = 0;
     for(int i=0; i < cycle; i++){
         cin >> temx;
         cin >> temy;
-        x = abs(x - temx);
-        y = abs(y - temy);
+        distance += abs(x - temx) + abs(y - temy);
+        x = temx;
+        y = temy;
     }
-    cout << x+y << endl;
+    cout << distance << endl;
     return 0;
 }
