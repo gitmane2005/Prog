23e78e798f5983b11476d6e51fe5c1f905a93653
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-03-08 22:05:23 +0000

    update

--- P04/trim.cpp
+++ P04/trim.cpp
@@ -2,28 +2,29 @@
 
 using namespace std;
 
-bool is_char(char a){
-    if( (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'b')) return true;
-    return false;
-}
 
 void trim(char s[]){
     int last_char_pos = 0;
     int first_char_pos = 0;
     bool char_found = false;
-    for(int i = 0; s[i] != '\n'; i++){
-        if(is_char(s[i]) && !char_found){
+    for(int i = 0; s[i] != '\0'; i++){
+        if(s[i] != ' ' && !char_found){
             first_char_pos = i;
             last_char_pos = i;
             char_found = true;
         }
-        else if (is_char(s[i]))
+        else if (s[i] != ' ')
         {
             last_char_pos = i;
         }
     }
-    for(int i = 0; i < last_char_pos; i++){
-        
+    if(char_found) {
+        for(int i = 0; i <= last_char_pos - first_char_pos; i++){
+            s[i] = s[i + first_char_pos];
+        }
+        s[last_char_pos - first_char_pos +1 ] = '\0';
     }
-
+    else{
+        s[0] = '\0';
+    }
 }
