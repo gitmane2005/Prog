4fc53dbd106dc9e56469f00138497ff12b7d2123
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-03-14 23:40:53 +0000

    update

--- test.cpp
+++ test.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-  char s[] = "";
-  cout << s << endl;
-  return 0;
+    int a[4] = {0,1,2,3};
+    int* b = new int[4] {3,2,1,0};
+    int i = 2;
+    //cin >> i;
+    if(--b[a[i]] != 0) delete []b;
+    return 0;
 }
