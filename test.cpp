cfcc2f2de2a637b9cb98c7d5959d6238fa95cff8
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-03-25 12:10:09 +0000

    update

--- test.cpp
+++ test.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <string> 
 using namespace std;
- 
-int main()
-{
-    vector<int> iv { 1, 2, 3 };
+string g = "xyz";
+
+
+int main () {
+    string inp; 
+    cin >> inp;
+    if (inp. length () < 2) {
+        string inp2 = "abc";
+        inp2.append(inp);
+        cout << inp2 << '\n';
+    }
+    else{
+        string* inp3 = new string ("ghi");
+        inp3 -> append(g) ;
+        cout << (*inp3) << '\n';
+    
+    }
     return 0;
 }
