87ab5a759bfe0624ee9db281a28dcb9126841f3c
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-04-14 17:51:40 +0100

    update

--- P08/date3.cpp
+++ P08/date3.cpp
@@ -10,11 +10,23 @@ Date::Date(int year, int month, int day){
     yyyymmdd = to_string(year) + to_string(month) + to_string(month);
 }
 Date::Date(const std::string& year_month_day){
-    
+    yyyymmdd = year_month_day.substr(0, 4) + year_month_day.substr(5, 7) + year_month_day.substr(9, 11);
+    if()
 }
     // accessors
-    int get_year() const;
-    int get_month() const;
-    int get_day() const;
+int Date::get_year() const{
+    return stoi(yyyymmdd.substr(0, 4));
+}
+int Date::get_month() const{
+    return stoi(yyyymmdd.substr(5, 7));
+}
+int Date::get_day() const{
+    return stoi(yyyymmdd.substr(9, 11));
+}
     // other  member functions
-    bool is_valid() const;
+bool Date::is_valid() const{
+    int year = stoi(yyyymmdd.substr(0, 4));
+    int month = stoi(yyyymmdd.substr(5, 7));
+    int day = stoi(yyyymmdd.substr(9, 11));
+
+}
--- test.cpp
+++ test.cpp
@@ -4,16 +4,7 @@
 #include <vector>
 using namespace std;
 int main () {
-    vector<string> a { "Portugal", "Spain", "France", "England" };
-    vector<string> b;
-    for (string& s : a) {
-        if (s.length() > 6) {
-            b.push_back(s);
-            s = "-";
-        }
-    }
-    for (string s: a) cout << s;
-    cout << '\n';
-    for (string s : b) cout << s;
-    cout << '\n';
+    string t = "23";
+    int value = t.to_integer()
+    cout << value;
 }
