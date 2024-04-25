120281654e789e24059ac9d2cc1316c77bafdba0
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-04-25 19:13:03 +0100

    update

--- P10/Power.h
+++ P10/Power.h
@@ -2,17 +2,19 @@
 #define POWER_H
 
 #include <iostream>
+#include <cmath>
 #include "Operation.h"
+using namespace std;
 
 class Power : public Operation{
 public:
     Power(int op1_, int op2_);
-    int operation();
+    int operation() const override;
 };
 
 Power::Power(int op1_, int op2_): Operation(op1_, op2_){}
-int Power::operation(){
-    return (get_op1() + get_op2());
+int Power::operation() const{
+    return pow(get_op1(), get_op2());
 }
 
 #endif
--- P10/Sum.h
+++ P10/Sum.h
@@ -3,15 +3,16 @@
 
 #include <iostream>
 #include "Operation.h"
+using namespace std;
 
 class Sum : public Operation{
 public:
     Sum(int op1_, int op2_);
-    int operation();
+    int operation() const override;
 };
 
 Sum::Sum(int op1_, int op2_): Operation(op1_, op2_){}
-int Sum::operation(){
+int Sum::operation() const{
     return (get_op1() + get_op2());
 }
 
